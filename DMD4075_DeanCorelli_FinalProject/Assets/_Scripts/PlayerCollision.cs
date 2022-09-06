26ea0de5d775291c0ef3315a7b12f7eab4bca9a2
Dean Corelli <dean.corelli@uconn.edu>
2022-09-06 18:47:12 -0400

    save

--- DMD4075_DeanCorelli_FinalProject/Assets/_Scripts/PlayerCollision.cs
+++ DMD4075_DeanCorelli_FinalProject/Assets/_Scripts/PlayerCollision.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
 
-  //  private void OnCollisionEnter2D(Collision2D collision)
+  // private void OnCollisionEnter2D(Collision2D collision)
    // {
-      //  if(collision.transform.tag == "Enemy")
-      //  {
-       //     PlayerHealth.health--;
+      //if(collision.transform.tag == "Enemy")
+       // {
+        //    PlayerHealth.health--;
        // }
-   // }
+  // }
 
 }
