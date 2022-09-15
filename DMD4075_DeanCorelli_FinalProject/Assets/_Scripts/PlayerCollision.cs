bb64164f1027dbc6477e6916ff66576c039feac7
Dean Corelli <dean.corelli@uconn.edu>
2022-09-15 11:03:08 -0400

    HeartHealthSystem

--- DMD4075_DeanCorelli_FinalProject/Assets/_Scripts/PlayerCollision.cs
+++ DMD4075_DeanCorelli_FinalProject/Assets/_Scripts/PlayerCollision.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Enemy")
+        {
+            PlayerHealth.health--;
+            if(PlayerHealth.health <= 0)
+            {
+                //PlayerManager.isGameOver = true;
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                StartCoroutine(GetHurt());
+            }
+        }
 
-  // private void OnCollisionEnter2D(Collision2D collision)
-   // {
-      //if(collision.transform.tag == "Enemy")
-       // {
-        //    PlayerHealth.health--;
-       // }
-  // }
+        if(collision.transform.tag == "slimeBall")
+        {
+            PlayerHealth.health--;
+        }
+    }
 
+    IEnumerator GetHurt()
+    {
+        Physics2D.IgnoreLayerCollision(6,9);
+        yield return new WaitForSeconds(3);
+        Physics2D.IgnoreLayerCollision(6, 9, false);
+    }
 }
--- DMD4075_DeanCorelli_FinalProject/Assets/_Scripts/PlayerHealth.cs
+++ DMD4075_DeanCorelli_FinalProject/Assets/_Scripts/PlayerHealth.cs
@@ -1,63 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public Rigidbody2D rb;
-    
-    public int health = 100;
-    int currentHealth;
 
-    
+    public static int health = 10;
+    public Image[] hearts;
 
-    public Animator animator; 
+    public Sprite fullHeart;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentHealth = health;
-    }
+    public Sprite emptyHeart;
 
-   // public void TakeDamage(int damage)
+   // private void OnCollisionExit2D(Collision2D collision)
    // {
-     // currentHealth -= 5;
-
-     // animator.SetTrigger("Pain");
-
-        //if (currentHealth <= 0)
-     //  {
-        //    Die();
-        //}
-   // }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.collider.tag == "Enemy")
-        {
-            health -= 5;
+       // if (collision.collider.tag == "Enemy")
+       // {
+          //  health -= 5;
             
-        }
-        
-    }
-
-
-
-    void Die()
-    {
-
-        //animator.SetBool("IsDead", true);
-
-
-        GetComponent<Collider2D>().enabled = false;
-        this.enabled = false;
-
-
-    }
+       // /}
+        //
+   // }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (Image img in hearts)
+        {
+            img.sprite = emptyHeart;
+        }
+        for (int i = 0; i < health; i++)
+        {
+            hearts[i].sprite = fullHeart;
+        }
     }
 }
