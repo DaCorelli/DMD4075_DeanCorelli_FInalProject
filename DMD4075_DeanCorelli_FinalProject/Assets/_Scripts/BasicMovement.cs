3f75fe8b5bf35ec7a736dcb152001e6eccf518d5
Dean Corelli <dean.corelli@uconn.edu>
2022-10-25 12:24:43 -0400

    Save

--- DMD4075_DeanCorelli_FinalProject/Assets/_Scripts/BasicMovement.cs
+++ DMD4075_DeanCorelli_FinalProject/Assets/_Scripts/BasicMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BasicMovement : MonoBehaviour
 {
@@ -31,7 +32,8 @@ public class BasicMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       // _rigidbody = GetComponent<Rigidbody2D>();
+        // _rigidbody = GetComponent<Rigidbody2D>();
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
