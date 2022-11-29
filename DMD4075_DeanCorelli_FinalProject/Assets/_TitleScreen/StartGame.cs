7881bdaa3edf766e8a2016887b3de5a69e174dd2
Dean Corelli <dean.corelli@uconn.edu>
2022-11-29 12:04:59 -0500

    Title

--- DMD4075_DeanCorelli_FinalProject/Assets/_TitleScreen/StartGame.cs
+++ DMD4075_DeanCorelli_FinalProject/Assets/_TitleScreen/StartGame.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+   // public void PlayGame()
+  //  {
+       // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   // }
+
     public string LevelName;
 
     public void LoadLevel()
